620ba4090bec64aea7325a9daeb7a293056c9005
Priyanshu Sharma <priyanshusharma6666@gmail.com>
2026-06-14 17:25:27 +0530

    feat: implement response handling and logging utilities for improved error management

---
+++ server/app/utils/logger.js
@@ -0,0 +1,62 @@
+const { createLogger: createWinstonLogger, transports, format } = require('winston');
+require('winston-daily-rotate-file');
+
+const appName = 'TaskDock';
+const writeLogsToFile = process.env.NODE_ENV !== 'production';
+
+const loggerConsole = createWinstonLogger({
+    transports: [
+        new transports.Console({
+            format: format.combine(format.colorize(), format.simple()),
+            level: 'info'
+        })
+    ]
+});
+
+let loggerFile;
+if (writeLogsToFile) {
+    loggerFile = createWinstonLogger({
+        transports: [
+            new transports.DailyRotateFile({
+                filename: `${appName}_%DATE%.log`,
+                datePattern: 'DDMMYYYY',
+                format: format.simple(),
+                level: 'info',
+                maxFiles: '3'
+            })
+        ]
+    });
+}
+
+const loggerToConsole = new Proxy({}, {
+    get: (_, level) => (message) => {
+        loggerConsole[level](`${level === 'error' ? 'SwanError: ' : ''}${message}`);
+    }
+});
+
+const loggerToFile = writeLogsToFile
+    ? new Proxy({}, {
+        get: (_, level) => (message) => {
+            loggerFile[level](`${new Date().toISOString()} ${message}`);
+        }
+    })
+    : new Proxy({}, { get: () => () => {} });
+
+const logger = new Proxy({}, {
+    get: (_, level) => (() => {
+        if (writeLogsToFile) {
+            return (message) => {
+                loggerToConsole[level](message);
+                loggerToFile[level](message);
+            };
+        }
+        return loggerToConsole[level];
+    })()
+});
+
+
+module.exports = {
+    logger,
+    loggerToConsole,
+    loggerToFile
+};
